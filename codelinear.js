45ae78bd39b2806bbb254cb6411bb29881b29081
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2023-01-24 20:37:48 +0530

    Add files via upload

---
+++ codelinear.js
@@ -0,0 +1,14 @@
+function logo(){
+    document.getElementById("logo").style.width="150px";
+    document.getElementById("circle").style.display="block";
+    document.getElementById("waae").style.display="block";
+    document.getElementById("hyphen").style.display="block";
+    document.getElementById("menu").style.top="-20px"
+}
+function logo1(){
+    document.getElementById("logo").style.width="132.41px";
+    document.getElementById("circle").style.display="none";
+    document.getElementById("waae").style.display="none";
+    document.getElementById("hyphen").style.display="none";
+    document.getElementById("menu").style.top="0px"
+}

c16c6f9c8a42f8f5f25a996584e779430d18459e
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2023-01-25 18:09:35 +0530

    Add files via upload

--- codelinear.js
+++ codelinear.js
@@ -1,12 +1,12 @@
 function logo(){
-    document.getElementById("logo").style.width="150px";
+    document.getElementById("logo").style.width="120px";
     document.getElementById("circle").style.display="block";
     document.getElementById("waae").style.display="block";
     document.getElementById("hyphen").style.display="block";
     document.getElementById("menu").style.top="-20px"
 }
 function logo1(){
-    document.getElementById("logo").style.width="132.41px";
+    document.getElementById("logo").style.width="98px";
     document.getElementById("circle").style.display="none";
     document.getElementById("waae").style.display="none";
     document.getElementById("hyphen").style.display="none";
